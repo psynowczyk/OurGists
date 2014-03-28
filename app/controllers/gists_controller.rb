dabfbe9ec9981571221fabd1997ebfd51b5c1247
Damian Labuda <kaka2991@gmail.com>
2014-03-28 21:29:24 +0100

    Poprawki dla obsługi infinite scrolling.

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -5,6 +5,10 @@ class GistsController < ApplicationController
   # GET /gists.json
   def index
     @gists = Gist.paginate(:page => params[:page], :per_page => 10)
+    respond_to do |format|
+        format.html
+        format.js
+    end
   end
 
   # GET /gists/1
