96d454b29a822c9da07e2378535a512eacfed7a4
Damian Labuda <kaka2991@gmail.com>
2014-04-26 21:58:54 +0200

    Added users authentication with sign up/logout navbar.
    Logowanie użytkowników jest możliwe za pomocą trzech metod:
    - Facebook,
    - GitHub,
    - Google.
    
    Apps klucze (public i secret) są przetrzymywane w env'ie heroku.
    
    Użyte gemy:
    - omniauth,
    - omniauth-facebook,
    - omniauth-github,
    - omniauth-google-oauth2.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140323094151) do
+ActiveRecord::Schema.define(version: 20140426081554) do
 
   create_table "gists", force: true do |t|
     t.text     "snippet"
     t.string   "lang"
     t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
