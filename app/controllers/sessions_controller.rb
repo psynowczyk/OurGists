04f3a7eb449e2fdb428534d4514ad726fdb50746
Damian Labuda <kaka2991@gmail.com>
2014-04-27 11:17:32 +0200

    Fixed login failed.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_filter :require_login
+
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
