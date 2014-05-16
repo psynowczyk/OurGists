91978363ba891a3ddd2419bbf2ed156de591ac42
Piotr Synowczyk <piotrsynowczyk@wp.pl>
2014-05-16 16:33:31 +0200

    zmiany kluczy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV["FB_KEY"], ENV["FB_SEC"]
+    provider :facebook, "579777852130347", "e439a024ca7cdc5b6a0e95494cff73db"
     provider :github, ENV["GH_KEY"], ENV["GH_SEC"]
     provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-    config.public_key  = ENV["RE_CAPTCHA_KEY"]
-    config.private_key = ENV["RE_CAPTCHA_SEC"]
+    config.public_key  = "6LejrPMSAAAAADuZ-J0Ig_tj1qEPYW1u-3lPF9B3"
+    config.private_key = "6LejrPMSAAAAAIt1qBX-e5l-HJouXyoSAmq66zfO"
 end
