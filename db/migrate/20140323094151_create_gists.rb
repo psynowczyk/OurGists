df72f868e153ad91144306dca3743924ad3ca0e8
Piotr Synowczyk <piotrsynowczyk@wp.pl>
2014-05-16 18:35:22 +0200

    zmiany controllera gistsow

--- db/migrate/20140323094151_create_gists.rb
+++ db/migrate/20140323094151_create_gists.rb
@@ -1,7 +1,7 @@
 class CreateGists < ActiveRecord::Migration
   def change
     create_table :gists do |t|
-	  t.integer :user_id
+	t.integer :user_id
       t.text :snippet
       t.string :lang
       t.string :description
