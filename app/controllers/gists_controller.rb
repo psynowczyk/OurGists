b03e7a9f70ebe12b0a324d213dac2db7fc141add
Piotr Synowczyk <piotrsynowczyk@wp.pl>
2014-05-16 17:10:15 +0200

    zmiany controllera gistsow

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -32,7 +32,7 @@ class GistsController < ApplicationController
   # POST /gists
   # POST /gists.json
   def create
-    @gist = @user.gists.new(gist_params)
+    @gist = Gist.new(gist_params)
     captcha_message = "The data you entered for the CAPTCHA wasn't correct. Please try again."
 
     respond_to do |format|
