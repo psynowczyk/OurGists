fc4418d215eab3f1075685554192c096e114f4ea
Piotr Synowczyk <piotrsynowczyk@wp.pl>
2014-05-17 21:23:07 +0200

    zmiany parametrow wyszukiwania

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -37,7 +37,6 @@ class GistsController < ApplicationController
 
     respond_to do |format|
       if current_user
-        @gist.user_id = current_user.id
         if @gist.save
             format.html { redirect_to @gist, notice: 'Gist was successfully created.' }
             format.json { render action: 'show', status: :created, location: @gist }
