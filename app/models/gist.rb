6e3b7187a1263c2eaf30bf6429497249451bf5c5
Piotr Synowczyk <piotrsynowczyk@wp.pl>
2014-05-17 20:37:55 +0200

    zmiany parametrow wyszukiwania

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -2,6 +2,6 @@ class Gist < ActiveRecord::Base
 	belongs_to :user
 	validates :snippet, :lang, :presence => true
 	def self.search(snippet_search,desc_search,lang_search,page)
-	paginate(:page => page, :per_page => 10, :conditions => ['user_id LIKE ? AND snippet LIKE ? AND description LIKE ? AND lang LIKE ?', "%#{snippet_search}%", "%#{desc_search}%", "%#{lang_search}%"], :order => 'updated_at DESC')
+	paginate(:page => page, :per_page => 10, :conditions => ['user_id LIKE ? AND snippet LIKE ? AND description LIKE ? AND lang LIKE ?', "%#{uid_search}%", "%#{snippet_search}%", "%#{desc_search}%", "%#{lang_search}%"], :order => 'updated_at DESC')
 	end
 end
